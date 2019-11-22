2c2e8e825215f73990b2d618dfcde7a6ddfb1f14
SHERMANOUKO <ouko.sherman@gmail.com>
2019-11-22 11:18:34 +0300

    Update requests.ts

--- requests.ts
+++ requests.ts
@@ -34,6 +34,44 @@ getPage(page: number) {
         map<any,any>(res => res.data)
     )
   }
+
+
+ //request to get a pdf file document from a django rest API I created
+ this.apiservice.getPdf('http://127.0.0.1:8000/getReport/document').subscribe(
+      (res)=>{
+        var newBlob = new Blob([res], { type: "application/pdf" });
+
+        // IE doesn't allow using a blob object directly as link href
+        // instead it is necessary to use msSaveOrOpenBlob
+        if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveOrOpenBlob(newBlob);
+            return;
+        }
+
+        // For other browsers: 
+        // Create a link pointing to the ObjectURL containing the blob.
+        const data = window.URL.createObjectURL(newBlob);
+
+        var link = document.createElement('a');
+        link.href = data;
+        link.download = "Je kar.pdf";
+        // this is necessary as link.click() does not work on the latest firefox
+        link.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }));
+
+        setTimeout(function () {
+            // For Firefox it is necessary to delay revoking the ObjectURL
+            window.URL.revokeObjectURL(data);
+            link.remove();
+        }, 100);
+      },
+      (err)=>{
+       console.log(err.status)
+       console.log(err.statusText)
+       console.log(err.error)
+      }
+    ) 
+    
+  }
   
 //<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12" *ngFor="let property of asyncProperties | async | paginate: { id: 'server', itemsPerPage: 6, currentPage: p, totalItems: totalCount }" id="imageholder">
 //  <div>
